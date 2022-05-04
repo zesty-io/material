37cc521e3c06f75e6200c7095860f31fe4d3c574
Andres <agalin920@gmail.com>
2022-05-04 09:56:23 -0700

    Initial setup and theme export

---
+++ src/theme/palette.ts
@@ -0,0 +1,38 @@
+import { PaletteOptions } from '@mui/material/styles';
+const palette: PaletteOptions = {
+  primary: {
+    main: '#404759',
+    contrastText: '#ffffff',
+  },
+  secondary: {
+    main: '#497edf',
+    contrastText: '#ffffff',
+  },
+  success: {
+    main: '#75bf25',
+    contrastText: '#ffffff',
+  },
+  warning: {
+    main: '#f17829',
+    contrastText: '#ffffff',
+  },
+  error: {
+    main: '#e53c05',
+    contrastText: '#ffffff',
+  },
+  // action: {
+  //   active: 'rgba(0,0,0,54)',
+  //   hover: 'rgba(0,0,0,0.04)',
+  //   hoverOpacity: 0.04,
+  //   selected: 'rgba(0,0,0,0.08)',
+  //   selectedOpacity: 0.08,
+  //   disabled: 'rgba(26, 32, 44, 0.4)',
+  //   disabledBackground: 'rgba(26, 32, 44, 0.12)',
+  //   disabledOpacity: 0.4,
+  //   focus: 'rgba(0,0,0,0.12)',
+  //   focusOpacity: 0.12,
+  //   activatedOpacity: 0.12,
+  // },
+};
+
+export default palette;
