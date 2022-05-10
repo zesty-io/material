0ec10c1487492607f578f531af556448a108fb14
Ryan Pals <ryan.pals@zesty.io>
2022-05-10 19:10:02 -0400

    Address feedback:
    Use more default styles
    Fix font size

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -1,25 +1,19 @@
 import { Components } from '@mui/material'
-import { PaletteOptions, SimplePaletteColorOptions } from '@mui/material/styles';
+import { SimplePaletteColorOptions } from '@mui/material/styles';
 import palette from './palette'
-import typography from './typography';
 
 
 const components: Components = {
     MuiTooltip: {
         styleOverrides: {
             tooltip: {
-               fontSize: '16px', // TODO: fix this, probably pull in size from typography.ts?
-               fontFamily: typography.fontFamily,
-               //TODO fix this typing
-               color: (palette?.primary as SimplePaletteColorOptions)?.contrastText,
-               backgroundColor: (palette?.primary as SimplePaletteColorOptions)?.main,
-
-               padding: "15px, 23px",
+               color: (palette.primary as SimplePaletteColorOptions).contrastText,
+               backgroundColor: (palette.primary as SimplePaletteColorOptions).main,
+               fontSize: "14px",
                lineHeight: "20px",
-               borderRadius: "4px",
             },
             arrow: {
-               color: (palette?.primary as SimplePaletteColorOptions)?.main,
+               color: (palette.primary as SimplePaletteColorOptions).main,
             }
         }
     }
