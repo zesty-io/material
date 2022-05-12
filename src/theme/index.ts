e46ed3271f7746ffd52c8b3f5a82f0289d5f1392
Ryan Pals <ryanjpals@gmail.com>
2022-05-12 12:08:39 -0400

    Mui tooltip theming (#3)
    * Add theming for MUI tooltip
    
    * Get rid of incorrect paddings
    
    * Address feedback:
    
    Use more default styles
    Fix font size
    
    * Compose theme in steps, remove components file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,9 +2,24 @@ import palette from './palette';
 import typography from './typography';
 import { createTheme, Theme } from '@mui/material/styles';
 
-const theme: Theme = createTheme({
+let theme: Theme = createTheme({
   palette,
   typography,
 });
 
+theme = createTheme(theme, {
+  MuiTooltip: {
+      styleOverrides: {
+          tooltip: {
+              color: theme.palette.primary.contrastText,
+              backgroundColor: theme.palette.primary.main,
+              fontSize: "14px",
+              lineHeight: "20px",
+          },
+          arrow: {
+              color: theme.palette.primary.main,
+          }
+      }
+  }
+})
 export default theme;
