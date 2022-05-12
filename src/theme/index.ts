82993d67200cc58c2f185d6ebc4c3887ffe20f16
Ryan Pals <ryan.pals@zesty.io>
2022-05-12 11:09:07 -0400

    Compose theme in steps, remove components file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,12 +1,25 @@
-import components from './components'
 import palette from './palette';
 import typography from './typography';
 import { createTheme, Theme } from '@mui/material/styles';
 
-const theme: Theme = createTheme({
-  components, 
+let theme: Theme = createTheme({
   palette,
   typography,
 });
 
+theme = createTheme(theme, {
+  MuiTooltip: {
+      styleOverrides: {
+          tooltip: {
+              color: theme.palette.primary.contrastText,
+              backgroundColor: theme.palette.primary.main,
+              fontSize: "14px",
+              lineHeight: "20px",
+          },
+          arrow: {
+              color: theme.palette.primary.main,
+          }
+      }
+  }
+})
 export default theme;
