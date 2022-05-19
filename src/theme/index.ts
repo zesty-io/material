25e9fc53b5752e5938718534154d2bb96270efb8
David Naimi <d88naimi@gmail.com>
2022-05-19 14:53:49 -0700

    added theme colors for secondary toggle button

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,7 @@
 import palette from './palette';
 import typography from './typography';
 import { createTheme, Theme } from '@mui/material/styles';
+import { ToggleButtonProps } from '@mui/material';
 
 let theme: Theme = createTheme({
   palette,
@@ -14,22 +15,34 @@ theme = createTheme(theme, {
         tooltip: {
           color: theme.palette.primary.contrastText,
           backgroundColor: theme.palette.primary.main,
-          fontSize: "14px",
-          lineHeight: "20px",
+          fontSize: '14px',
+          lineHeight: '20px',
         },
         arrow: {
           color: theme.palette.primary.main,
-        }
-      }
+        },
+      },
     },
     MuiInputBase: {
       styleOverrides: {
         root: {
-          backgroundColor: theme.palette.primary.contrastText
+          backgroundColor: theme.palette.primary.contrastText,
         },
-      }
-    }
-  }
-})
+      },
+    },
+    MuiToggleButton: {
+      styleOverrides: {
+        root: ({ ownerState, theme }: any) => ({
+          ...(ownerState.selected && {
+            color: `${theme.palette[ownerState.color].contrastText} !important`,
+            backgroundColor: `${
+              theme.palette[ownerState.color].main
+            } !important`,
+          }),
+        }),
+      },
+    },
+  },
+});
 
 export default theme;
