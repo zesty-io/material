6817a4955c8bfd3569af002ff4a6ab165ee6f3ff
Andres <agalin920@gmail.com>
2022-06-09 17:40:43 -0700

    Field type one to many updates

--- src/FieldTypeOneToMany/FieldTypeOneToMany.stories.tsx
+++ src/FieldTypeOneToMany/FieldTypeOneToMany.stories.tsx
@@ -1,7 +1,6 @@
-import { SyntheticEvent, useState } from 'react';
+import { ReactNode, SyntheticEvent, useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import FieldTypeOneToMany, { FieldTypeOneToManyProps } from '.';
-import { randomOptions } from '../utils/virtualization';
 
 export default {
   title: 'FieldTypeOneToMany',
@@ -10,16 +9,19 @@ export default {
 } as Meta;
 
 const Template: Story<FieldTypeOneToManyProps> = (args) => {
-  const [value, setValue] = useState<string[]>([]);
+  const [value, setValue] = useState<{component: string | ReactNode, value: string, inputLabel: string}[]>([]);
 
-  const [options, setOptions] = useState<string[]>([]);
+  const [options, setOptions] = useState<{component: string | ReactNode, value: string, inputLabel: string}[]>([]);
+
 
   const handleOnOpen = async () => {
+    const largeArr = new Array(1000).fill(null);
     await new Promise((resolve) => setTimeout(resolve, 3000))
-    setOptions(randomOptions);
+    const data = largeArr.map((_, idx) => ({component: <div>{`Test ${idx}`}</div>, value: String(Math.random()), inputLabel: `Test ${idx}`}));
+    setOptions(data);
   }
 
-  const handleOnChange = (e: SyntheticEvent<Element, Event>, values: string[]) => {
+  const handleOnChange = (e: SyntheticEvent<Element, Event>, values: {component: string | ReactNode, value: string, inputLabel: string}[]) => {
     setValue(values);
   }
 
@@ -36,11 +38,9 @@ const Template: Story<FieldTypeOneToManyProps> = (args) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  textFieldProps: {
-    placeholder: 'Placeholder Text...',
-    label: 'OneToOne label',
-    helperText: 'OneToOne helperText',
-  }
+  placeholder: 'Placeholder Text...',
+  label: 'OneToMany label',
+  helperText: 'OneToMany helperText',
 };
 
 
