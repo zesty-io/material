538d957a7a1dda1e6bb3da692d38a5813958b7c0
Ryan Pals <ryan.pals@zesty.io>
2022-06-10 17:16:43 -0500

    Center icon in textfield label

--- src/FieldTypeText/index.tsx
+++ src/FieldTypeText/index.tsx
@@ -16,7 +16,7 @@ const FieldTypeText = ({label, maxLength = 150, value, helperText, required, ...
     <FormControl fullWidth required={required}>
       {/* Sets flex order to make appended required asterisk properly positioned */}
       <FormLabel sx={{display: 'flex', justifyContent: 'space-between', '& .MuiFormLabel-asterisk': {order: 2}}}>
-        <Box sx={{ order: 1}}>{label}</Box> 
+        <Box sx={{ order: 1, display: 'flex', alignItems: 'center' }}>{label}</Box>
         <Box sx={{ order: 3, flex: 1, textAlign: 'right'}}>{value?.length}/{maxLength}</Box>
       </FormLabel>
       <MuiTextField
