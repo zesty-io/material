2ab09905eb2cdccb9b835d6fcd3c4ea827383032
David Naimi <d88naimi@gmail.com>
2022-06-17 09:29:24 -0700

    fixing overflow of copybutton

--- src/CopyButton/index.tsx
+++ src/CopyButton/index.tsx
@@ -4,18 +4,18 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import CheckIcon from '@mui/icons-material/Check';
 
 export interface CopyButtonProps extends ButtonProps {
-   /**
+  /**
    * Value to be copied to clipboard
    */
-  value: string ;
+  value: string;
 }
 
-const CopyButton = ({value, sx, ...props }: CopyButtonProps) => {
+const CopyButton = ({ value, sx, ...props }: CopyButtonProps) => {
   const [copied, setCopied] = useState(false);
 
   const copyValue = useCallback(() => {
-      navigator?.clipboard.writeText(value);
-      setCopied(true);
+    navigator?.clipboard.writeText(value);
+    setCopied(true);
   }, [value]);
 
   useEffect(() => {
@@ -29,26 +29,33 @@ const CopyButton = ({value, sx, ...props }: CopyButtonProps) => {
   }, [copied]);
 
   return (
-      <Button
-        variant='outlined'
-        onClick={copyValue}
-        size="small"
-        title={props.children ? `Copy ${props.children}` : `Copy ${value}`}
-        sx={{
-          textTransform: 'unset',
-          // Spread sx prop at the end to allow sx prop overrides
-          ...sx,
-        }}
-        startIcon={copied ? (
+    <Button
+      variant='outlined'
+      onClick={copyValue}
+      size='small'
+      title={props.children ? `Copy ${props.children}` : `Copy ${value}`}
+      sx={{
+        textTransform: 'unset',
+        width: '100%',
+        textOverflow: 'ellipsis',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+
+        // Spread sx prop at the end to allow sx prop overrides
+        ...sx,
+      }}
+      startIcon={
+        copied ? (
           <CheckIcon color='success' fontSize='small' />
         ) : (
           <ContentCopyIcon fontSize='small' />
-        )}
-        // Spread props at the end to allow prop overrides
-        {...props}
-      >
-        {props.children ? props.children : value}
-      </Button>
+        )
+      }
+      // Spread props at the end to allow prop overrides
+      {...props}
+    >
+      {props.children ? props.children : value}
+    </Button>
   );
 };
 
