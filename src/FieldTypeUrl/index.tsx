d4259650a002e32d0618f164a8edf17d81b2925d
Andres <agalin920@gmail.com>
2022-06-28 11:58:13 -0700

    remove unused imports

--- src/FieldTypeUrl/index.tsx
+++ src/FieldTypeUrl/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import MuiTextField, { OutlinedTextFieldProps } from '@mui/material/TextField';
 import { FormControl, FormLabel, Box } from '@mui/material';
 
