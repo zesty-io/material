c70b2b21f518778b4afd97a874167d17f984398a
Andres <agalin920@gmail.com>
2022-08-03 13:23:19 -0700

    remove commented code

--- src/LegacyTheme/palette.ts
+++ src/LegacyTheme/palette.ts
@@ -20,19 +20,6 @@ const palette: PaletteOptions = {
     main: '#e53c05',
     contrastText: '#ffffff',
   },
-  // action: {
-  //   active: 'rgba(0,0,0,54)',
-  //   hover: 'rgba(0,0,0,0.04)',
-  //   hoverOpacity: 0.04,
-  //   selected: 'rgba(0,0,0,0.08)',
-  //   selectedOpacity: 0.08,
-  //   disabled: 'rgba(26, 32, 44, 0.4)',
-  //   disabledBackground: 'rgba(26, 32, 44, 0.12)',
-  //   disabledOpacity: 0.4,
-  //   focus: 'rgba(0,0,0,0.12)',
-  //   focusOpacity: 0.12,
-  //   activatedOpacity: 0.12,
-  // },
 };
 
 export default palette;
