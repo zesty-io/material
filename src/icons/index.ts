b34036a69bf0e8fcec8a48674a076955dafe11dc
Andres <agalin920@gmail.com>
2022-11-14 22:58:56 -0800

    Add database icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { FolderGlobal } from "./FolderGlobal";
 export { FlipHorizontal } from "./FlipHorizontal";
 export { FlipVertical } from "./FlipVertical";
+export { Database } from "./Database";
