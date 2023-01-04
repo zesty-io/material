080e23a1a77a5e956a1d605d7d4a9e329ade5ad8
Nar Cuenca <narc.ph@gmail.com>
2023-01-04 14:54:36 +0800

    task: add markdown and onetoone icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,5 @@ export { FolderGlobal } from "./FolderGlobal";
 export { FlipHorizontal } from "./FlipHorizontal";
 export { FlipVertical } from "./FlipVertical";
 export { Database } from "./Database";
+export { Markdown } from "./Markdown";
+export { OneToOne } from "./OneToOne";
