41ddc26d2e05de076764f7ce0cc3fbf9bbc6d56c
Nar Cuenca <narc.ph@gmail.com>
2023-07-21 15:39:42 +0800

    task: added new menu dropdown icon

---
+++ src/icons/MenuListDropDown.tsx
@@ -0,0 +1,10 @@
+import { SvgIcon, SvgIconProps } from "@mui/material";
+
+export const Brain = (props: SvgIconProps) => (
+  <SvgIcon {...props}>
+    <path
+      d="M8.33031 11.8617L11.2159 14.6815C11.6504 15.1062 12.3523 15.1062 12.7868 14.6815L15.6724 11.8617C16.3743 11.1758 15.873 10 14.8814 10H9.11021C8.11863 10 7.62841 11.1758 8.33031 11.8617Z"
+      fill="currentColor"
+    />
+  </SvgIcon>
+);
